2962fca71d1c4db153d6146165840fc65f31c28a
jadzon <jacobtoja@icloud.com>
2024-02-25 02:57:53 +0100

    Added book struct, pozdro

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import "fmt"
 
+type book struct {
+	title string
+	price float64
+}
+
 func newFunc(random int) int {
 	return random * 2
 }
 func main() {
 	fmt.Println("Hello World!")
 	fmt.Println(newFunc(2))
+	myBook := book{title: "Ksiazka trapu", price: 11.77}
+	fmt.Printf("Cena: %.2f, tytuł: %s", myBook.price, myBook.title)
 }
